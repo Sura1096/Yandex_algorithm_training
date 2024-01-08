fdcf9962ad3b01f636d3c28b37bdb3a1229febb8
Sura <suri.murodova@gmail.com>
2024-01-08 14:07:33 +0300

    Add solution

--- Second_lection/G-Greatest_product_of_two_nums.py
+++ Second_lection/G-Greatest_product_of_two_nums.py
@@ -19,3 +19,31 @@
 Вывод
 -5 -4
 '''
+
+
+def greatest_product_of_two():
+    nums = list(map(int, input().split()))
+    
+    maxi1 = float('-inf')
+    maxi2 = float('-inf')
+    mini1 = float('inf')
+    mini2 = float('inf')
+    for num in nums:
+        if num > maxi1:
+            maxi2 = maxi1
+            maxi1 = num
+        elif num > maxi2:
+            maxi2 = num
+        if num < mini1:
+            mini2 = mini1
+            mini1 = num
+        elif num < mini2:
+            mini2 = num
+
+    if maxi1 * maxi2 > mini1 * mini2:
+        return str(maxi2) + ' ' + str(maxi1)
+    else:
+        return str(mini1) + ' ' + str(mini2)
+
+
+print(greatest_product_of_two())
